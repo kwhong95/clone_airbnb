7a9484ce703c8ad5abad39c180966baf383dc9ed
kwhong95 <kwhong95@kakao.com>
2022-08-10 17:11:34 +0900

    회원가입과 로그인/ 회원가입 API

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -10,8 +10,9 @@ import { dayList, monthList, yearList } from "lib/staticDate";
 import palette from "styles/palette";
 import Selector from "components/common/Selector";
 import Button from "components/common/Button";
+import { signupAPI } from "lib/api/auth";
 
-const Container = styled.div`
+const Container = styled.form`
   width: 568px;
   padding: 32px;
   background-color: white;
@@ -114,8 +115,28 @@ const SignUpModal = () => {
     setBirthYear(event.target.value);
   };
 
+  //* 회원가입 폼 제출하기
+  const onSubmitSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const signUpBody = {
+        email,
+        lastname,
+        firstname,
+        password,
+        birthday: new Date(
+          `${birthYear}-${birthMonth!.replace("월", "")}-${birthDay}`
+        ).toISOString(),
+      };
+      await signupAPI(signUpBody);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <Container>
+    <Container onSubmit={onSubmitSignUp}>
       <CloseXIcon className="modal-close-x-icon" />
       <div className="input-wrapper">
         <Input
---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import Data from "lib/data";
+import { StoredUserType } from "types/user";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  //* 1. api method가 POST인지 확인
+  if (req.method === "POST") {
+    //* 2. req.body에 필요한 값이 전부 들어 있는지 확인
+    const { email, firstname, lastname, password, birthday } = req.body;
+    if (!email || !firstname || !lastname || !password || !birthday) {
+      res.statusCode = 400;
+      return res.send("필수 데이터가 없습니다.");
+    }
+
+    //* 3. email이 중복인지 확인
+    const userExist = Data.user.exist({ email });
+    if (userExist) {
+      res.statusCode = 409;
+      res.send("이미 가입된 이메일입니다.");
+    }
+
+    //* 4. 패스워드 암호화
+    const hasedPassword = bcrypt.hashSync(password, 8);
+
+    //* 5. 유저 정보 추가
+    const users = Data.user.getList();
+    let userId;
+    if (users.length === 0) {
+      userId = 1;
+    } else {
+      userId = users[users.length - 1].id + 1;
+    }
+    const newUser: StoredUserType = {
+      id: userId,
+      email,
+      firstname,
+      lastname,
+      password: hasedPassword,
+      birthday,
+      profileImage: "/static/image/user/default_user_profile_image.jpg",
+    };
+
+    Data.user.write([...users, newUser]);
+
+    //* 6. 추가된 유저의 정보와 token 전달
+    await new Promise((resolve) => {
+      const token = jwt.sign(String(newUser.id), process.env.JWT_SECRET!);
+      res.setHeader(
+        "Set-Cookie",
+        `access_token=${token}; path=/; expires=${new Date(
+          Date.now() + 60 * 60 * 24 * 1000 * 3 // 3day
+        )}; httponly`
+      );
+      resolve(token);
+    });
+
+    return res.end();
+  }
+  res.statusCode = 405;
+
+  return res.end();
+};
