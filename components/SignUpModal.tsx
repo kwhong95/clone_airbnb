9ab41a1ffe13c800e8a1f85e0c99372c6aa9cf01
kwhong95 <kwhong95@kakao.com>
2022-08-10 11:43:43 +0900

    리액트 포털을 활용하여 모달 훅스 만들기

---
+++ components/SignUpModal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 568px;
+  height: 614px;
+  background-color: white;
+`;
+
+const SignUpModal = () => {
+  return <Container>Sign Up</Container>;
+};
+
+export default SignUpModal;
