5a155a22e05a48b838940be576a2d15dda38d038
kwhong95 <kwhong95@kakao.com>
2022-08-11 12:14:36 +0900

    회원가입과 로그인/ 로그인 유지하기(버그 예상)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import App, { AppContext, AppProps } from "next/app";
+import axios from "lib/api";
 import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
 import { wrapper } from "store";
-import axios from "lib/api";
-import { userActions } from "store/user";
+import { cookieStringToObject } from "lib/utils";
+import { meAPI } from "lib/api/auth";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
@@ -18,8 +19,20 @@ const app = ({ Component, pageProps }: AppProps) => {
 
 app.getInitialProps = async (context: AppContext) => {
   const appInitialProps = await App.getInitialProps(context);
+  const cookieObject = cookieStringToObject(context.ctx.req?.headers.cookie);
   const { store } = context.ctx;
-  // const { isLogged } = store.getState().user;
+  const { isLogged } = store.getState().user;
+  console.log(cookieObject);
+
+  try {
+    if (!isLogged && cookieObject.access_token) {
+      axios.defaults.headers.cookie = cookieObject.access_token;
+      const { data } = await meAPI();
+      console.log(data);
+    }
+  } catch (e) {
+    console.log(e);
+  }
 
   return { ...appInitialProps };
 };
