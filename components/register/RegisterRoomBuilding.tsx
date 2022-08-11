99e2017810163e1201871a8381f567dc33ca48de
kwhong95 <kwhong95@kakao.com>
2022-08-11 14:16:25 +0900

    숙소 등록하기/ 건물 유형 셀렉터 생성

--- components/register/RegisterRoomBuilding.tsx
+++ components/register/RegisterRoomBuilding.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
+import { useSelector } from "store";
+import Selector from "components/common/Selector";
+import { largeBuildingTypeList } from "lib/staticDate";
 import palette from "styles/palette";
+import { useDispatch } from "react-redux";
+import { registerRoomActions } from "store/registerRoom";
 
 const Container = styled.div`
   padding: 62px 30px 100px;
@@ -14,13 +19,113 @@ const Container = styled.div`
     color: ${palette.gray_76};
     margin-bottom: 6px;
   }
+
+  .register-room-building-selector-wrapper {
+    width: 320px;
+    margin-bottom: 32px;
+  }
 `;
 
+// * 선택 불가능한 큰 범위 건물 유형
+const disabledlargeBuildingTypeOptions = ["하나를 선택해주세요."];
+
 const RegisterRoomBuilding: React.FC = () => {
+  const largeBuildingType = useSelector(
+    (state) => state.registerRoom.largeBuildingType
+  );
+  const buildingType = useSelector((state) => state.registerRoom.buildingType);
+
+  const dispatch = useDispatch();
+
+  //* 큰 범위 건물 유형 변경 시
+  const onChangeLargeBuildingType = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    dispatch(registerRoomActions.setLargeBuildingType(event.target.value));
+  };
+
+  //* 상세 건물 유형 변경 시
+  const onChangeBuildingType = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    dispatch(registerRoomActions.setBuildingType(event.target.value));
+  };
+
+  //* 선택된 건물 유형 options
+  const detailBuildingOptions = useMemo(() => {
+    switch (largeBuildingType) {
+      case "아파트": {
+        const { apartmentBuildingTypeList } = require("../../lib/staticDate");
+        dispatch(
+          registerRoomActions.setBuildingType(apartmentBuildingTypeList[0])
+        );
+        return apartmentBuildingTypeList;
+      }
+      case "주택": {
+        const { houstBuildingTypeList } = require("../../lib/staticDate");
+        dispatch(registerRoomActions.setBuildingType(houstBuildingTypeList[0]));
+        return houstBuildingTypeList;
+      }
+      case "별채": {
+        const {
+          secondaryUnitBuildingTypeList,
+        } = require("../../lib/staticDate");
+        dispatch(
+          registerRoomActions.setBuildingType(secondaryUnitBuildingTypeList[0])
+        );
+        return secondaryUnitBuildingTypeList;
+      }
+      case "독특한 숙소": {
+        const { uniqueSpaceBuildingTypeList } = require("../../lib/staticDate");
+        dispatch(
+          registerRoomActions.setBuildingType(uniqueSpaceBuildingTypeList[0])
+        );
+        return uniqueSpaceBuildingTypeList;
+      }
+      case "B&B": {
+        const { bnbBuildingTypeList } = require("../../lib/staticDate");
+        dispatch(registerRoomActions.setBuildingType(bnbBuildingTypeList[0]));
+        return bnbBuildingTypeList;
+      }
+      case "부티크호텔": {
+        const {
+          boutiquesHotelBuildingTypeList,
+        } = require("../../lib/staticDate");
+        dispatch(
+          registerRoomActions.setBuildingType(boutiquesHotelBuildingTypeList[0])
+        );
+        return boutiquesHotelBuildingTypeList;
+      }
+      default:
+        return [];
+    }
+  }, [largeBuildingType]);
+
   return (
     <Container>
       <h2>등록할 숙소의 종류는 무엇인가요?</h2>
       <h3>1단계</h3>
+      <div className="register-room-building-selector-wrapper">
+        <Selector
+          type="register"
+          value={largeBuildingType || undefined}
+          defaultValue="하나를 선택해주세요."
+          disabledOptions={disabledlargeBuildingTypeOptions}
+          label="우선 범위를 좁혀볼까요?"
+          options={largeBuildingTypeList}
+          onChange={onChangeLargeBuildingType}
+        />
+      </div>
+      <div className="register-room-building-selector-wrapper">
+        <Selector
+          type="register"
+          value={undefined}
+          disabled={!largeBuildingType}
+          label="건물 유형을 선택하세요."
+          options={detailBuildingOptions}
+          onChange={onChangeBuildingType}
+        />
+      </div>
     </Container>
   );
 };
