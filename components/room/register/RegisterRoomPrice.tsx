55e1aa4009223fe8c50f356aa173fefcadbe2dbf
kwhong95 <kwhong95@kakao.com>
2022-08-16 12:00:08 +0900

    숙소 등록하기 STEP10 / 숙소 요금 설정하기

---
+++ components/room/register/RegisterRoomPrice.tsx
@@ -0,0 +1,62 @@
+import Input from "components/common/Input";
+import { makeMoneyString } from "lib/utils";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "store";
+import { registerRoomActions } from "store/registerRoom";
+import styled from "styled-components";
+import palette from "styles/palette";
+import RegisterRoomFooter from "./RegisterRoomFooter";
+
+const Container = styled.div`
+  padding: 62px 30px 100px;
+  h2 {
+    font-size: 19px;
+    font-weight: 800;
+    margin-bottom: 56px;
+  }
+  h3 {
+    font-weight: bold;
+    color: ${palette.gray_76};
+    margin-bottom: 6px;
+  }
+`;
+
+const RegisterRoomPrice = () => {
+  const dispatch = useDispatch();
+
+  const price = useSelector((state) => state.registerRoom.price);
+
+  //* 금액 변경 시
+  const onChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const input = event.target.value;
+
+    //? 인풋 값이 비워지면 price를 0으로 변경
+    if (!input) {
+      dispatch(registerRoomActions.setPrice(0));
+    }
+
+    const numberPrice = Number(input.replace(/,/g, ""));
+    if (numberPrice) {
+      dispatch(registerRoomActions.setPrice(numberPrice));
+    }
+  };
+
+  return (
+    <Container>
+      <h2>숙소 요금 설정하기</h2>
+      <h3>10단계</h3>
+      <Input
+        label="기본요금"
+        value={makeMoneyString(String(price))}
+        onChange={onChangePrice}
+      />
+      <RegisterRoomFooter
+        prevHref="/room/register/title"
+        nextHref="/room/register/data"
+      />
+    </Container>
+  );
+};
+
+export default RegisterRoomPrice;
---
+++ pages/room/register/price.tsx
@@ -0,0 +1,9 @@
+import RegisterRoomPrice from "components/room/register/RegisterRoomPrice";
+import { NextPage } from "next";
+import React from "react";
+
+const price: NextPage = () => {
+  return <RegisterRoomPrice />;
+};
+
+export default price;
