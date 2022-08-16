8dc691e52cb311fdc8770c89689ab30bfc7d18ab
kwhong95 <kwhong95@kakao.com>
2022-08-16 11:17:07 +0900

    숙소 등록하기 STEP8 / 숙소 설명 리덕스 설정

--- store/registerRoom.ts
+++ store/registerRoom.ts
@@ -24,6 +24,7 @@ type RegisterRoomState = {
   amentities: string[];
   conveniences: string[];
   photos: string[];
+  description: string;
 };
 
 //* 초기 상테
@@ -72,6 +73,8 @@ const initialState: RegisterRoomState = {
   conveniences: [],
   //* 숙소 사진
   photos: [],
+  //* 숙소 설명
+  description: "",
 };
 
 const registerRoom = createSlice({
@@ -202,6 +205,10 @@ const registerRoom = createSlice({
     //* 숙소 사진 변경하기
     setPhotos(state, action: PayloadAction<string[]>) {
       state.photos = action.payload;
+    },
+    //* 숙소 설명 변경하기
+    setDescription(state, action: PayloadAction<string>) {
+      state.description = action.payload;
     },
   },
 });
